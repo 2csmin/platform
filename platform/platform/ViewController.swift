d15a3a931cac6b8ffff532a5ac1b6953b041c703
2csmin <163493839+2csmin@users.noreply.github.com>
2024-04-07 20:33:35 +0900

    Add files via upload

---
+++ platform/platform/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  platform
+//
+//  Created by 조수민 on 4/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
